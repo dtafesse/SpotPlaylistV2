390b3aafb97e456054fc7ffc18419a5fed9910b4
dtafesse <dtafesse@uwm.edu>
2018-11-11 04:06:11 -0600

    removed unused function in client/src/api/index.js

--- functions/client/src/api/index.js
+++ functions/client/src/api/index.js
@@ -37,18 +37,6 @@ export default {
     }).then(response => response.json());
   },
 
-  // temp way to fetch artwork until search by top rated tracks of artists in implement
-  fetchArtwork() {
-    const id = '6NijWPXUijjGcrdkQFcflv';
-    return fetch(`/server/api/artwork/${id}`, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      }
-    }).then(response => response.json());
-  },
-
   fetchResultsForQuery(query) {
     return fetch(`${ROOT_URL}/api/search/${query}`, {
       method: 'GET',
