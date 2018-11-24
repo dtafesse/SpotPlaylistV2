8993c0cc00b6e7c44fb121bff4bb15d4b14b6089
dtafesse <dtafesse@uwm.edu>
2018-11-24 10:39:53 -0600

    routing changes

--- functions/client/src/components/NavBar/nav.vue
+++ functions/client/src/components/NavBar/nav.vue
@@ -2,19 +2,19 @@
     <div>
         <v-navigation-drawer temporary v-model="sideNav" clipped app>
             <v-list dense>
-               <v-list-tile v-for="item in menuItem" :key="item.title" @click="routeItem(item)">
-                   <v-list-tile-action>
+                <v-list-tile v-for="item in menuItem" :key="item.title" @click="routeItem(item)">
+                    <v-list-tile-action>
                         <v-icon> {{ item.icon }} </v-icon>
-                   </v-list-tile-action>
-                   <v-list-tile-content> {{ item.title }} </v-list-tile-content>
-               </v-list-tile>
+                    </v-list-tile-action>
+                    <v-list-tile-content> {{ item.title }} </v-list-tile-content>
+                </v-list-tile>
             </v-list>
         </v-navigation-drawer>
         
         <v-toolbar fixed flat dark color="primary">
             <v-toolbar-side-icon @click.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
             <v-toolbar-title class="title mr-4">
-                <router-link to="/" tag="span" style="cursor: pointer">{{ title }}</router-link>
+                <router-link to="/saved/playlists" tag="span" style="cursor: pointer">{{ title }}</router-link>
             </v-toolbar-title>
             
             <!-- SEARCH BAR -->
@@ -25,7 +25,7 @@
                     flat v-for="item in menuItem" :key="item.title" :to="item.link" @click="routeItem(item)"
                 >
                     <v-icon left dark>{{ item.icon }}</v-icon>
-                     {{item.title}}
+                        {{item.title}}
                 </v-btn>
             </v-toolbar-items>
 
@@ -36,64 +36,69 @@
 
 <script>
 // import searchBar from './searchBar';
-import searchBar from './searchTextField';
+import searchBar from "./searchTextField";
 
 export default {
-    name: 'navBar',
-    components: {
-        searchBar
+  name: "navBar",
+  components: {
+    searchBar
+  },
+  data() {
+    return {
+      sideNav: false,
+      hideTitle: false
+    };
+  },
+  computed: {
+    menuItem() {
+      let menuItemIcons = [
+        { icon: "lock_open", title: "Sign In", link: "/signin" },
+        { icon: "face", title: "Sign Up", link: "/signup" }
+      ];
+
+      if (this.userIsAuth) {
+        menuItemIcons = [
+          { icon: "exit_to_app", title: "Logout", link: "/signin" }
+        ];
+      }
+      return menuItemIcons;
     },
-    data(){
-        return {
-            sideNav: false,    
-            hideTitle: false    
-        }
+    userIsAuth() {
+      return (
+        this.$store.getters.user !== null &&
+        this.$store.getters.user !== undefined
+      );
     },
-    computed: {
-        menuItem(){
-            let menuItemIcons = [
-                { icon: 'lock_open', title: 'Sign In', link: '/signin' },
-                { icon: 'face', title: 'Sign Up', link: '/signup'}
-            ];
-            
-            if(this.userIsAuth) {
-                menuItemIcons = [
-                    { icon: 'exit_to_app', title: 'Logout', link: '/signin'}
-                ];
-            }
-            return menuItemIcons;
-        },
-        userIsAuth() {
-            return this.$store.getters.user !== null && this.$store.getters.user !== undefined
-        },
-        title(){
-            return this.hideTitle && this.$vuetify.breakpoint.xs? '' : 'SpotPlaylist';
-        }
-    },
-    methods: {
-        onHideTitle(value){
-            this.hideTitle = value;
-        },
-        routeItem(item){
-            if(item.title === "Sign In"){
-                this.signIn();
-            }else if(item.title === "Sign Up"){
-                this.signUp();
-            }else{
-                this.logout();
-            }
-        },
-        signIn() {
-            this.$router.push({path: '/signin'});
-        },
-        signUp() {
-            this.$router.push({path: '/signup'});
-        },
-        logout() {
-            this.$store.dispatch('logout');
-            // inside of the dispatched func, this.$router.push({path: '/landing'});
-        }
+    title() {
+      return this.hideTitle && this.$vuetify.breakpoint.xs
+        ? ""
+        : "SpotPlaylist";
     }
-}
+  },
+  methods: {
+    onHideTitle(value) {
+      this.hideTitle = value;
+    },
+    routeItem(item) {
+      if (item.title === "Sign In") {
+        this.signIn();
+      } else if (item.title === "Sign Up") {
+        this.signUp();
+      } else {
+        this.logout();
+      }
+    },
+    signIn() {
+      this.$router.push({ path: "/signin" });
+    },
+    signUp() {
+      this.$router.push({ path: "/signup" });
+    },
+    logout() {
+      this.$store.dispatch("logout");
+      // inside of the dispatched func, this.$router.push({path: '/landing'});
+    }
+  }
+};
 </script>
 
