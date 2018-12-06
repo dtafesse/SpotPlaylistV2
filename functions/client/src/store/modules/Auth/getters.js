d2bc076bbf1e33296e9d49b8f8e5c4e4f45ce5c5
dtafesse <dtafesse@uwm.edu>
2018-12-05 19:31:24 -0600

    fixed off bug with getter that checks if spotify account is linked

--- functions/client/src/store/modules/Auth/getters.js
+++ functions/client/src/store/modules/Auth/getters.js
@@ -1,5 +1,5 @@
 const getters = {
-  isSpotifyLoggedIn: state => !!state.spotifyAuthAccessCode,
+  isSpotifyLoggedIn: state => !!state.spotifyAuthAccessCode && !!state.user,
   user: state => state.user,
   error: state => state.error,
   getAccessToken: state => state.spotifyAuthAccessCode,
