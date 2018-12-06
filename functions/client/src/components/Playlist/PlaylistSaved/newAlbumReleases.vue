0964503385d5bb26c1757632d08922299f49a9e3
dtafesse <dtafesse@uwm.edu>
2018-12-05 22:43:50 -0600

    used CardList component in newAlbumReleases Component

--- functions/client/src/components/Playlist/PlaylistSaved/newAlbumReleases.vue
+++ functions/client/src/components/Playlist/PlaylistSaved/newAlbumReleases.vue
@@ -1,9 +1,31 @@
 <template>
-  <v-flex xs12 sm6></v-flex>
+  <card-list
+    :items="newlyReleasedAlbums"
+    heading="New Album Releases"
+    :numOfPagesVisible="6"
+    :pageSizeMobile="4"
+    :pageSizeNonMobile="6"
+    @onClickItem="handleSelectedAlbum"
+  />
 </template>
 
 <script>
+import CardList from "../../Shared/CardList";
+
 export default {
-  name: "newAlbumReleases"
+  name: "newAlbumReleases",
+  components: {
+    CardList
+  },
+  computed: {
+    newlyReleasedAlbums() {
+      return this.$store.getters.getNewlyReleasedAlbums;
+    }
+  },
+  methods: {
+    handleSelectedAlbum(index) {
+      console.log(this.newlyReleasedAlbums[index].name);
+    }
+  }
 };
 </script>
